43863cd1220eec745a83514c7cb4c0b812ffd41e
Gabriela Pacheco Vieira <bela.vieira.pacheco@gmail.com>
2025-07-21 16:28:11 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //vai ter operacoes estaticas para instanciar os daos
@@ -9,6 +10,10 @@ public class DaoFactory {
 	//retornar um seller dao
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
